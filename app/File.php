7bd10eac095a2bd2143e45e082ca8af6c55f75c8
yemiwebby <yemiwebby@gmail.com>
2018-02-15 12:36:14 +0100

    Initial Commit

---
+++ app/File.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+
+    protected $fillable = [
+        'name',  'type', 'extension', 'user_id'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+
+
+Route::get('/', 'MainController@index')->name('home')->middleware('auth');
+Route::get('/files/{type}/{id?}', 'FileController@index');
+
+Route::post('files/add', 'FileController@store');
+Route::post('files/edit/{id}', 'FileController@edit');
+Route::post('files/delete/{id}', 'FileController@destroy');
+
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
